ec465e5fbdae0e596fb1128b9c5d6d6e63b1ea4b
Jan Charler <charlerjan@gmail.com>
2020-09-21 12:12:58 +0100

    Tidy Timer.h and Timer.cpp class files

--- FP/Timer.cpp
+++ FP/Timer.cpp
@@ -1,1 +1,37 @@
 #include "Timer.h"
+
+void Timer::delay(const std::chrono::milliseconds& ms) {
+	std::unique_lock<std::mutex> lck(mtx);
+	_completed = false;
+
+	_isRunning = true;
+
+	lck.unlock();
+	auto time_started = steady_clock::now();
+
+	std::this_thread::sleep_for(ms);
+
+	lck.lock();
+	_isRunning = false;
+	_completed = true;
+}
+
+bool Timer::isRunning() {
+	std::unique_lock<std::mutex> lck(mtx);
+	return _isRunning;
+}
+
+bool Timer::isCompleted() {
+	std::unique_lock<std::mutex> lck(mtx);
+	return _completed;
+}
+
+bool Timer::start(const std::chrono::milliseconds& ms) {
+	if (isRunning()) {
+		return false;
+	}
+	else {
+		_ftr = std::async(&Timer::delay, this, ms);
+		return true;
+	}
+}
--- FP/Timer.h
+++ FP/Timer.h
@@ -17,45 +17,12 @@ private:
 
 	bool _isRunning;
 	bool _completed;
-
-	void delay(const std::chrono::milliseconds& ms) {
-		std::unique_lock<std::mutex> lck(mtx);
-		_completed = false;
-
-		_isRunning = true;
-
-		lck.unlock();
-		auto time_started = steady_clock::now();
-
-		std::this_thread::sleep_for(ms);
-
-		lck.lock();
-		_isRunning = false;
-		_completed = true;
-	}
+	void delay(const std::chrono::milliseconds& ms);
 
 public:
 	Timer() : _isRunning(false), _completed(false) {};
 
-	bool isRunning() {
-		std::unique_lock<std::mutex> lck(mtx);
-		return _isRunning;
-	}
-
-	bool isCompleted() {
-		std::unique_lock<std::mutex> lck(mtx);
-		return _completed;
-	}
-
-	// @brief: Starts a timer in a separate thread. Read from isCompleted() to check if timer successfully stopped running.
-	// @return: True if timer successfully started. False if time is already running.
-	bool start(const std::chrono::milliseconds& ms) {
-		if (isRunning()) {
-			return false;
-		}
-		else {
-			_ftr = std::async(&Timer::delay, this, ms);
-			return true;
-		}
-	}
+	bool isRunning();
+	bool isCompleted();
+	bool start(const std::chrono::milliseconds& ms);
 };
